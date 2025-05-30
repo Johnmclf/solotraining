6cbc631385d00ef1e20f7cd35d4efcd0eb92f24b
Johnmclf <johnmclf@gmail.com>
2025-05-30 14:17:01 +0200

    change import

--- public/import.php
+++ public/import.php
@@ -1,16 +1,16 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=mysql;dbname=railway", "root", getenv('DB_PASSWORD'));
+    $pdo = new PDO("mysql:host=mysql;dbname=solotraining", "root", "solo5263");
 
-    // Modification de la colonne id
+    // Modifier la colonne 'id' pour ajouter AUTO_INCREMENT
     $sql = "ALTER TABLE users 
-            MODIFY id INT NOT NULL AUTO_INCREMENT, 
-            ADD PRIMARY KEY (id)";
-    
+            MODIFY id INT NOT NULL AUTO_INCREMENT";
+
     $pdo->exec($sql);
 
-    echo " La colonne 'id' a bien été modifiée en AUTO_INCREMENT avec PRIMARY KEY.";
+    echo " La colonne 'id' est maintenant en AUTO_INCREMENT.";
 } catch (PDOException $e) {
     echo " Erreur : " . $e->getMessage();
 }
 ?>
+
