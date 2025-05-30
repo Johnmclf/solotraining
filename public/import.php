5143923dff59fb9a5f39d048cca99acf25966fa6
Johnmclf <johnmclf@gmail.com>
2025-05-30 13:36:19 +0200

    add import

---
+++ public/import.php
@@ -0,0 +1,16 @@
+<?php
+try {
+    $pdo = new PDO("mysql:host=mysql;dbname=solotraining", "root", "solo5263");
+
+    // Modification de la colonne id
+    $sql = "ALTER TABLE users 
+            MODIFY id INT NOT NULL AUTO_INCREMENT, 
+            ADD PRIMARY KEY (id)";
+    
+    $pdo->exec($sql);
+
+    echo " La colonne 'id' a bien été modifiée en AUTO_INCREMENT avec PRIMARY KEY.";
+} catch (PDOException $e) {
+    echo " Erreur : " . $e->getMessage();
+}
+?>
