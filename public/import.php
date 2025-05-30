bee46ae67c13b3f31d72a5c900b72efee69cae13
Johnmclf <johnmclf@gmail.com>
2025-05-30 14:21:15 +0200

    change import

--- public/import.php
+++ public/import.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=mysql;dbname=solotraining", "root", getenv('DB_PASSWORD'));
+    $pdo = new PDO("mysql:host=mysql;dbname=railway", "root", getenv('DB_PASSWORD'));
 
     // Modifier la colonne 'id' pour ajouter AUTO_INCREMENT
     $sql = "ALTER TABLE users 
