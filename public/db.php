91c541383ddbcf0729d4c3b74d026e2603fcc77a
Johnmclf <johnmclf@gmail.com>
2025-07-06 22:15:49 +0200

    use supabase for db

--- public/db.php
+++ public/db.php
@@ -5,11 +5,10 @@
 #$user = 'root';
 #$port = '3306';
 #$pass = getenv('DB_PASSWORD');
-$host = 'dpg-d1l8q9er433s73de3omg-a';
-$db = 'solo_training_db';
-$user = 'solo_training_db_user';
+$host = 'db.vxztonwsxzzjrixgomjv.supabase.co';
+$db = 'postgres';
+$user = 'postgres';
 $port = '5432';
-$pass = 'deYxYhzAFRUXMsGYi2c7mVpyMX3AaGBH';
+$pass = 'W0u726ACdDYflPkv';
 
 date_default_timezone_set('Europe/Paris');
-
