ed6f700543398f3be300171724ffbcff27ce542a
Johnmclf <johnmclf@gmail.com>
2025-07-06 17:00:16 +0200

    change railway for render

--- public/db.php
+++ public/db.php
@@ -1,10 +1,15 @@
 <?php
 
-$host = 'mysql';
-$db = 'railway';
-$user = 'root';
-$port = '3306';
-$pass = getenv('DB_PASSWORD');
+#$host = 'mysql';
+#$db = 'railway';
+#$user = 'root';
+#$port = '3306';
+#$pass = getenv('DB_PASSWORD');
+$host = 'dpg-d1l8q9er433s73de3omg-a';
+$db = 'solo_training_db';
+$user = 'solo_training_db_user';
+$port = '5432';
+$pass = 'deYxYhzAFRUXMsGYi2c7mVpyMX3AaGBH';
 
 date_default_timezone_set('Europe/Paris');
 
