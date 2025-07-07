7ce9e94ff67b36db5425df79ac81c85d3cf868d1
Johnmclf <johnmclf@gmail.com>
2025-07-07 17:43:03 +0200

    Version 2

--- public/db.php
+++ public/db.php
@@ -12,15 +12,3 @@ $port = '6543';
 $pass = 'W0u726ACdDYflPkv';
 
 date_default_timezone_set('Europe/Paris');
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$db";
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 5,
-    ]);
-    echo "Connexion réussie à la base Supabase PostgreSQL !";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
