109f8a9f737bde479983443832d0fad3ad817e71
Johnmclf <johnmclf@gmail.com>
2025-07-07 17:30:47 +0200

    correct username db

--- public/db.php
+++ public/db.php
@@ -7,7 +7,7 @@
 #$pass = getenv('DB_PASSWORD');
 $host = 'aws-0-eu-west-3.pooler.supabase.com';
 $db = 'postgres';
-$user = 'postgres';
+$user = 'postgres.vxztonwsxzzjrixgomjv';
 $port = '6543';
 $pass = 'W0u726ACdDYflPkv';
 
