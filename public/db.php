95502899faf63dde948d0170566a02f229a7e2f4
Johnmclf <johnmclf@gmail.com>
2025-07-07 17:16:56 +0200

    update info.php

--- public/db.php
+++ public/db.php
@@ -5,10 +5,10 @@
 #$user = 'root';
 #$port = '3306';
 #$pass = getenv('DB_PASSWORD');
-$host = 'db.vxztonwsxzzjrixgomjv.supabase.co';
+$host = 'aws-0-eu-west-3.pooler.supabase.com';
 $db = 'postgres';
 $user = 'postgres';
-$port = '5432';
+$port = '6543';
 $pass = 'W0u726ACdDYflPkv';
 
 date_default_timezone_set('Europe/Paris');
